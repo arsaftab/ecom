99597b19417ceef5d417370a9c394fdab016c316
arsaftab <98858494+arsaftab@users.noreply.github.com>
2022-11-03 15:08:39 -0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,9 @@ use App\Http\Controllers\Admin\CategoryController;
 use App\Http\Controllers\Admin\ColorController;
 use App\Http\Controllers\Admin\ProductController;
 use App\Http\Middleware\AdminMiddleware;
+use App\Models\User;
+use Illuminate\Support\Facades\View;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -21,14 +24,24 @@ use App\Http\Middleware\AdminMiddleware;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('welcome');
 
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
+Route::post('user_active', [DashboardController::class, 'is_active'])->name('user_active');
+Route::post('product-color-update', [ProductController::class, 'updateProdClrQty'])->name('prod_clr_upd');
+Route::post('product-color-delete', [ProductController::class, 'deleteProdClrQty'])->name('prod_clr_dlt');
+
+
 Route::prefix('/admin')->middleware(['auth', 'isAdmin'])->group(function(){
+    // $users = User::where('is_active',0)->get();
+    // View::share('users', $users);
+
     Route::get('dashboard', [DashboardController::class, 'index']);
+    
+    Route::get('users/{user}/{notification_id}', [DashboardController::class, 'show'])->name('users.show');
     
     //category routes
     Route::get('category', [CategoryController::class, 'index']);
@@ -45,6 +58,7 @@ Route::prefix('/admin')->middleware(['auth', 'isAdmin'])->group(function(){
     Route::get('products/{product}/edit', [ProductController::class, 'edit']);
     Route::put('products/{product}', [ProductController::class, 'update'])->name('products.update');
     Route::delete('products/{product}', [ProductController::class, 'destroy'])->name('products.destroy');
+
 
     Route::resource('colors', ColorController::class);
     
